906d72d824579f96d745e534c5b5e8404b10b1f8
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-05-22 11:58:43 -0700

    if applied, this commit allows for initial users to be populaed dynamically with correct Money Format

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,6 +9,32 @@ const calculateBtn = document.querySelector("#calculate-wealth");
 //initialize array for data
 let data = [];
 
+//format number as Money
+function formatMoney(number){
+    const Regex = "/\d(?=(\d{3})+\.)/g, '$&,'";
+    return '$' + (number).toFixed(2).replace(Regex);
+}
+
+//Updates DOM 
+function updateDOM(providedData = data){
+    //Clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    //loops through array of data
+    providedData.forEach(function(person){
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${person.name}</strong> ${formatMoney(person.money)}`;
+        main.appendChild(element);
+    });
+}
+
+//Adds User Data
+function addUserData(obj){
+    data.push(obj);
+    updateDOM();
+}
+
 //Fetch Random User and Add Money
 async function getRandomUser(){
     const API = 'https://randomuser.me/api';
@@ -22,6 +48,14 @@ async function getRandomUser(){
         money: Math.floor(Math.random() * 100000)
     };
 
-    addUserBtn(newUser);
-
+    addUserData(newUser);
 }
+
+
+//Event Listeners
+//window.addEventListener('DOMContentLoaded', getRandomUser);
+
+//calls
+getRandomUser();
+getRandomUser();
+getRandomUser();
